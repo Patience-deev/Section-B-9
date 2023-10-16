9ccf3e0578807e8dd47772f4fe069f7a24c5ec0e
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-16 14:06:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_B_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test("FizzBuzz"));
+            Console.WriteLine(test("Fizz"));
+            Console.WriteLine(test("Buzz"));
+            Console.WriteLine(test("Founder"));
+            Console.ReadLine();
+        }
+        public static string test(string str)
+        {
+            if (str.StartsWith("F") && str.EndsWith("B"))
+            {
+                return "FizzBuzz";
+            }
+            else if (str.StartsWith("F"))
+            {
+                return "Fizz";
+            }
+            else if (str.EndsWith("B"))
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return str;
+            }
+        }
+    }
+}
